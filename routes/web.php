fa53a1036b131e770ae3616ba10318c12b3358ef
chinghuihui <sunny70624@gmail.com>
2021-09-04 13:45:52 +0800

    newsletters

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/newsletter', function () {
+    request()->validate([
+        'email' => 'required|email'
+    ]);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us5'
+    ]);
+
+    try {
+        $mailchimp->lists->addListMember('76e0489e0c', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+
+
+    return redirect('/posts')->with('success', 'You are now signed up for our newsletter!');
+});
+
 
 Route::get('/welcome', function () {
     return view('welcome',  ['user' => 'hui']);
