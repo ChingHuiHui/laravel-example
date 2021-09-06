e070bde2bc3e0edc99bf57fd436480732b6838cd
chinghuihui <sunny70624@gmail.com>
2021-09-06 12:45:37 +0800

    storage/image

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,11 +1,15 @@
 <x-layout>
-    <section>
-        <x-panel class="max-w-sm mx-auto">
-            <form action="/admin/posts" method="POST">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
+        <x-panel>
+            <form action="/admin/posts" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-6">
                     <label for="title" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title"
+                        value="{{ old('title') }}" required>
 
                     @error('title')
                     <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -13,7 +17,18 @@
                 </div>
                 <div class="mb-6">
                     <label for="slug" class="block mb-2 uppercase font-bold text-xs text-gray-700">Slug</label>
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="slug" id="slug" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="slug" id="slug"
+                        value="{{ old('slug') }}" required>
+
+                    @error('slug')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label for="thumbnail"
+                        class="block mb-2 uppercase font-bold text-xs text-gray-700">Thumbnail</label>
+                    <input class="border border-gray-400 p-2 w-full" type="file" name="thumbnail" id="thumbnail"
+                        value="{{ old('thumbnail') }}" required>
 
                     @error('slug')
                     <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -21,7 +36,8 @@
                 </div>
                 <div class="mb-6">
                     <label for="exceprt" class="block mb-2 uppercase font-bold text-xs text-gray-700">Excerpt</label>
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="excerpt" id="excerpt" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="excerpt" id="excerpt"
+                        value="{{ old('excerpt') }}" required>
 
                     @error('excerpt')
                     <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -30,17 +46,19 @@
                 <div class="mb-6">
                     <label for="body" class="block mb-2 uppercase font-bold text-xs text-gray-700">Body</label>
                     <textarea class="border border-gray-400 p-2 w-full" name="body" id="body" required>
+                    {{ old('body') }}
                     </textarea>
 
                     @error('body')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    <p class=" text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="mb-6">
                     <label for="category_id" class="block mb-2 uppercase font-bold text-xs text-gray-700">Title</label>
-                    <select name="category_id" id="category_id">
+                    <select name="category_id" id="category_id" required>
                         @foreach (\App\Models\Category::all() as $category)
-                        <option value="{{$category->id}}">{{ ucwords($category->name) }}</option>
+                        <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                            {{ ucwords($category->name) }}</option>
                         @endforeach
                     </select>
 
