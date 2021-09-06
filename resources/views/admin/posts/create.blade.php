6f60167f1a0e41c07317cefe9e911ff23b9a3ff9
chinghuihui <sunny70624@gmail.com>
2021-09-06 14:00:10 +0800

    aside

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,30 +1,25 @@
 <x-layout>
-    <section class="py-8 max-w-md mx-auto">
-        <h1 class="text-lg font-bold mb-4">
-            Publish New Post
-        </h1>
-        <x-panel>
-            <form action="/admin/posts" method="POST" enctype="multipart/form-data">
-                @csrf
-                <x-form.input name="title" />
-                <x-form.input name="slug" />
-                <x-form.input name="thumbnail" type="file" />
-                <x-form.input name="exceprt" />
-                <x-form.textarea name="body" />
-                <x-form.field>
-                    <x-form.label name="category_id" />
-                    <select name="category_id" id="category_id" required>
-                        @foreach (\App\Models\Category::all() as $category)
-                        <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
-                            {{ ucwords($category->name) }}</option>
-                        @endforeach
-                    </select>
+    <x-setting heading="Publish New Post">
+        <form action="/admin/posts" method="POST" enctype="multipart/form-data">
+            @csrf
+            <x-form.input name="title" />
+            <x-form.input name="slug" />
+            <x-form.input name="thumbnail" type="file" />
+            <x-form.input name="exceprt" />
+            <x-form.textarea name="body" />
+            <x-form.field>
+                <x-form.label name="category_id" />
+                <select name="category_id" id="category_id" required>
+                    @foreach (\App\Models\Category::all() as $category)
+                    <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
+                        {{ ucwords($category->name) }}</option>
+                    @endforeach
+                </select>
 
-                    <x-form.error name="category_id" />
-                </x-form.field>
+                <x-form.error name="category_id" />
+            </x-form.field>
 
-                <x-form.button>Publish</x-form.button>
-            </form>
-        </x-panel>
-    </section>
+            <x-form.button>Publish</x-form.button>
+        </form>
+    </x-setting>
 </x-layout>
--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,7 +2,7 @@
 
 <x-form.field>
     <x-form.label name="{{$name}}" />
-    <input class="border border-gray-400 p-2 w-full" type="{{$type}}" name="{{ $name }}" id="{{ $name }}"
+    <input class="border border-gray-200 p-2 w-full" type="{{$type}}" name="{{ $name }}" id="{{ $name }}"
         value="{{ old($name) }}" required />
 
     <x-form.error name="{{$name}}" />
